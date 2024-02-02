665786f5cc3f1fc7c71c27c063453e48b62a02b7
Robin <robinvandernoord@gmail.com>
2024-02-02 13:25:38 +0100

    Feat: added --language (default Dutch) and --verbose

--- insanely-fast-whisper.py
+++ insanely-fast-whisper.py
@@ -3,19 +3,32 @@
 import click
 import os
 import time
+import sys
+
+def log(*text: str, verbose: bool, **kw):
+    if not verbose:
+        return
+
+    kw['file'] = sys.stderr
+    print(*text, **kw)
 
 @click.command()
 @click.option('--model', default='openai/whisper-base', help='ASR model to use for speech recognition. Default is "openai/whisper-base". Model sizes include base, small, medium, large, large-v2. Additionally, try appending ".en" to model names for English-only applications (not available for large).')
 @click.option('--device', default='cuda:0', help='Device to use for computation. Default is "cuda:0". If you want to use CPU, specify "cpu".')
+@click.option('--language', default='dutch', help='Language to transcribe text for.')
 @click.option('--dtype', default='float32', help='Data type for computation. Can be either "float32" or "float16". Default is "float32".')
 @click.option('--batch-size', type=int, default=8, help='Batch size for processing. This is the number of audio files processed at once. Default is 8.')
 @click.option('--better-transformer', is_flag=True, help='Flag to use BetterTransformer for processing. If set, BetterTransformer will be used.')
+@click.option('--verbose', is_flag=True, help='Flag to enable debug logging.')
 @click.option('--chunk-length', type=int, default=30, help='Length of audio chunks to process at once, in seconds. Default is 30 seconds.')
 @click.argument('audio_file', type=str)
-def asr_cli(model, device, dtype, batch_size, better_transformer, chunk_length, audio_file):
+def asr_cli(model, device, language, dtype, batch_size, better_transformer, verbose, chunk_length, audio_file):
+    log("Starting imports.", verbose=verbose)
     from transformers import pipeline
     import torch
 
+    log("Start loading model.", verbose=verbose)
+    
     # Initialize the ASR pipeline
     pipe = pipeline("automatic-speech-recognition",
                     model=model,
@@ -23,12 +36,13 @@ def asr_cli(model, device, dtype, batch_size, better_transformer, chunk_length, 
                     torch_dtype=torch.float16 if dtype == "float16" else torch.float32)
 
     if better_transformer:
+        log("Converting to Better Transformer.", verbose=verbose)
         pipe.model = pipe.model.to_bettertransformer()
 
     # Perform ASR
     click.echo("Model loaded.")
     start_time = time.perf_counter()
-    outputs = pipe(audio_file, chunk_length_s=chunk_length, batch_size=batch_size, return_timestamps=True)
+    outputs = pipe(audio_file, chunk_length_s=chunk_length, batch_size=batch_size, return_timestamps=True, generate_kwargs={"language": language})
 
     # Output the results
     click.echo(outputs)
